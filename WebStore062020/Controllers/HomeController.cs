c3f1bbf6fc32b7546589656ccd61528e02174b85
mamizerov <arsenal74@mail.ru>
2020-06-28 14:59:48 +0300

    ДЗ к первому уроку выполнено

--- WebStore062020/Controllers/HomeController.cs
+++ WebStore062020/Controllers/HomeController.cs
@@ -3,14 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore062020.Models;
 
 namespace WebStore062020.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly List<Employee> _Employees = new List<Employee>
         {
-            return View();
+            new Employee
+            {
+                Id = 1,
+                Age = 23,
+                Birthday = "20.10.1997",
+                SurName = "Гикбрейнс",
+                FirstName = "Павел",
+                SecondName = "Викторович"
+            },
+            new Employee
+            {
+                Id = 2,
+                Age = 33,
+                Birthday = "02.04.1987",
+                SurName = "Зайцев",
+                FirstName = "Александр",
+                SecondName = "Сергеевич"
+            },
+            new Employee
+            {
+                Id = 3,
+                Age = 43,
+                Birthday = "15.01.1977",
+                SurName = "Ловчев",
+                FirstName = "Владимир",
+                SecondName = "Алексеевич"
+            },
+
+        };
+
+
+        public IActionResult Index() => View();
+
+        public IActionResult Employees()
+        {
+            var employees = _Employees;
+            return View(employees);
         }
+
+        public IActionResult EmployeeInfo(int id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
+        }
+
     }
 }
