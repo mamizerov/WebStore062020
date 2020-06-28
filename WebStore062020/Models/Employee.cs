f54a8fa757ca0089e0c9a78d3322d7919a7d5ce9
mamizerov <arsenal74@mail.ru>
2020-06-28 14:28:31 +0300

    Создал модель

---
+++ WebStore062020/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore062020.Models
+{
+    public class Employee
+    {
+        public int Id {get; set;}
+
+        public string SurName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
