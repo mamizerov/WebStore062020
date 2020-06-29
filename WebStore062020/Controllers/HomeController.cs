35ea9a602e3fc664fbf8c00f2f28527e22d3750d
mamizerov <arsenal74@mail.ru>
2020-06-29 18:22:36 +0300

    ДЗ к уроку 2 выполнено, без редактирования\удаления сотрудников

--- WebStore062020/Controllers/HomeController.cs
+++ WebStore062020/Controllers/HomeController.cs
@@ -12,7 +12,23 @@ namespace WebStore062020.Controllers
 
         public IActionResult Index() => View();
 
+        public IActionResult Blogs() => View();
 
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult Error404() => View();
 
 
     }
