25cd5de9b82b9fa849b5b58aaac4d13a3f8f8413
mamizerov <arsenal74@mail.ru>
2020-07-18 22:23:02 +0300

    Сделана админка и управление товаром

--- WebStore062020/Startup.cs
+++ WebStore062020/Startup.cs
@@ -94,12 +94,21 @@ namespace WebStore062020
             app.UseAuthentication();
             app.UseAuthorization();
 
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name:"default",
                     pattern: "{controller=Home}/{action=Index}/{ID?}");
             });
+
         }
     }
 }
