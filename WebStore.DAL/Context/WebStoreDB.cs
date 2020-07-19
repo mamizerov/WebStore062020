e47c4f7e55777d9f45af53f43c61da72ea1d5731
mamizerov <arsenal74@mail.ru>
2020-07-19 17:30:24 +0300

    Созданы таблицы Orders OrdersItems в БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
 
 namespace WebStore.DAL.Context
 {
@@ -20,6 +21,10 @@ namespace WebStore.DAL.Context
         public DbSet<Brand> Brands { get; set; }
 
         public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
     }
